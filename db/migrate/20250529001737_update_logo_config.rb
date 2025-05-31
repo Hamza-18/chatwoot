369ceeb1524c1602d8cbe4b74899e1e1d005bfab
Hamza-18 <hamzabashir18@outlook.com>
2025-05-31 00:25:27 -0700

    issue-2 added comments

--- db/migrate/20250529001737_update_logo_config.rb
+++ db/migrate/20250529001737_update_logo_config.rb
@@ -1,11 +1,13 @@
 class UpdateLogoConfig < ActiveRecord::Migration[7.1]
   def up
+    # Update the logo configurations to point to the new custom logo path
     %w[LOGO_THUMBNAIL LOGO LOGO_DARK].each do |name|
       config = InstallationConfig.find_by(name: name)
-      if config
-        config.serialized_value = { value: '/brand-assets/custom_logo.svg' }.with_indifferent_access
-        config.save!
-      end
+      next unless config
+
+      # Update the serialized value to point to the new custom logo path using with_indifferent_access
+      config.serialized_value = { value: '/brand-assets/custom_logo.svg' }.with_indifferent_access
+      config.save!
     end
 
     GlobalConfig.clear_cache if defined?(GlobalConfig)
